3ca2bac562409f5f060a2d4e1814ba48ef5b8714
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-16 22:40:24 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 function mayor(numero1,numero2,numero3){
-    if (numero1>=numero2 && numero2 >=numero3){
-        let resultado= "El numero mayor es "+numero1;
-        return resultado;
-    } else if (numero2>=numero1 && numero1>=numero3){
-        let resultado= "El numero mayor es "+numero2;
-        return resultado;
-    } else{
-        let resultado= "El numero mayor es "+numero3;
-        return resultado;
+    /* Limpiar h1 */
+    document.querySelector("#mayor").innerHTML="";
+    document.querySelector("#menor").innerHTML="";
+    document.querySelector("#iguales").innerHTML="";
+    if (numero1>numero2 && numero2 >=numero3){
+        document.querySelector("#mayor").innerHTML= "El numero mayor es "+numero1;
+        if (numero2>numero3){
+            document.querySelector("#menor").innerHTML= "El numero menor es "+numero2;
+        } else{
+            document.querySelector("#menor").innerHTML= "El numero menor es "+numero3;
+        }
+
+    } else if (numero2>numero1 && numero2 >=numero3){
+        document.querySelector("#mayor").innerHTML= "El numero mayor es "+numero2;
+        if (numero1>numero3){
+            document.querySelector("#menor").innerHTML= "El numero menor es "+numero3;
+        } else{
+            document.querySelector("#menor").innerHTML= "El numero menor es "+numero1;
+        }
+    } else if (numero3>numero1 && numero3 >=numero2){
+        document.querySelector("#mayor").innerHTML= "El numero mayor es "+numero3;
+        if (numero1>numero2){
+            document.querySelector("#menor").innerHTML= "El numero menor es "+numero2;
+        } else{
+            document.querySelector("#menor").innerHTML= "El numero menor es "+numero1;
+        }
+    } else {
+        document.querySelector("#iguales").innerHTML= "Todos los numeros son iguales "+numero1;
     }
+
 }
 addEventListener("DOMContentLoaded", (e)=>{
     let calcular = document.querySelector("#form");
@@ -17,8 +37,7 @@ addEventListener("DOMContentLoaded", (e)=>{
         let numero1 = document.querySelector("#numero1").value;
         let numero2= document.querySelector("#numero2").value;
         let numero3 = document.querySelector("#numero3").value;
-        let resultado = mayor(numero1,numero2,numero3); 
-        document.querySelector("#res").innerHTML= resultado;
+        mayor(numero1,numero2,numero3); 
     })
     
 })
