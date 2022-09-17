47099d79c39feab0f0577a4d1fd40bf4f6b84ea0
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-16 22:22:09 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-function positivo(numero){
-    if (numero>0 && numero<100){
-        let resultado="El numero es positivo y es menor a 100";
+function mayor(numero1,numero2,numero3){
+    if (numero1>=numero2 && numero2 >=numero3){
+        let resultado= "El numero mayor es "+numero1;
+        return resultado;
+    } else if (numero2>=numero1 && numero1>=numero3){
+        let resultado= "El numero mayor es "+numero2;
+        return resultado;
+    } else{
+        let resultado= "El numero mayor es "+numero3;
         return resultado;
     }
-    else{ 
-        let resultado="El numero o es negativo o es mayor a 100";
-        return resultado;
-    }
-
 }
 addEventListener("DOMContentLoaded", (e)=>{
     let calcular = document.querySelector("#form");
     calcular.addEventListener("submit", (e)=>{
         e.preventDefault();
-        let numero = document.querySelector("#numero").value;
-        let resultado = positivo(numero); 
+        let numero1 = document.querySelector("#numero1").value;
+        let numero2= document.querySelector("#numero2").value;
+        let numero3 = document.querySelector("#numero3").value;
+        let resultado = mayor(numero1,numero2,numero3); 
         document.querySelector("#res").innerHTML= resultado;
     })
     
