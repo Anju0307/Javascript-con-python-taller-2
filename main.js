3869ed2c8ebbe011f09d6f6a0b4542696e0e6f85
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-19 07:31:26 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 addEventListener("DOMContentLoaded", (e) => {
-    let boton = document.querySelector("#form");
-    boton.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let num1 = document.querySelector("#num1").value;
-        let num2 = document.querySelector("#num2").value;
-        if (num1 > num2){
-            let res = num1-num2;
-            document.querySelector("#res").innerHTML = `El resultado de restar el primer numero y el segundo es ${res}`;
-        } else{
-            document.querySelector("#res").innerHTML = "La operacion no se puede hacer :("
-        }
-
+    let dato = document.querySelector("#dato");
+    dato.addEventListener("change", (e) => {
+        let selec = dato.value;
+        let boton = document.querySelector("#form");
+        boton.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let can = document.querySelector("#can").value;
+            const pre = 2000;
+            let total;
+            if (selec == "TipoA") {
+                total = ((pre * can) - ((pre * can) * 0.3));
+            } else if (selec == "TipoB") {
+                total = ((pre * can) - ((pre * can) * 0.2));
+            } else if (selec == "TipoC") {
+                total = ((pre * can) - ((pre * can) * 0.1));
+            } else {
+                alert("Escoge un tipo de helado!!!!")
+            }
+            document.querySelector("#total").innerHTML = `El total a pagar es de ${total}`
+        })
     })
 })
