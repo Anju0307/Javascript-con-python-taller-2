2ee0bfe32343e832cf852ee9bd3fd79bdcdfb2ae
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-18 22:26:49 -0500

    Ejercicio 9

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 addEventListener("DOMContentLoaded", (e) => {
-    let dato = document.querySelector("#dato");
-    dato.addEventListener("change", (e) => {
-        let seleccionado = dato.value;
-        let boton = document.querySelector("#form");
-        boton.addEventListener("submit", (e) =>{
-            e.preventDefault();
-            let sueldoEmp = pagar(seleccionado);
-            document.querySelector("#total").innerHTML = sueldoEmp;
-        })
+    let boton = document.querySelector("#form");
+    boton.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let num1 = document.querySelector("#num1").value;
+        let num2 = document.querySelector("#num2").value;
+        let num3 = document.querySelector("#num3").value;
+        let num4 = document.querySelector("#num4").value;
+        validacion(num1,num2,num3,num4);
+        document.querySelector("#pares").innerHTML= `La suma de los numeros pares es: ${pares}`;
+        document.querySelector("#impares").innerHTML= `La multiplicacion de los numeros impares es: ${impares}`;
     })
 })
+let impares=1, pares=0;
 
-function pagar (seleccionado){
-    let horas = document.querySelector("#horas").value;
-    if  (seleccionado == "Planta"){
-        const sueldo = 20000;
-        let pagoTotal = sueldo*horas;
-        return pagoTotal;
-    } else if (seleccionado == "Administrativo"){
-        const sueldo = 10000;
-        let pagoTotal = sueldo*horas;
-        return pagoTotal;
-    } else{
-        alert("Debes seleccionar un cargo")
+function validacion(num1,num2,num3,num4){
+    if (num1 % 2 != 0){
+        impares*=parseInt (num1);       
+    } else {
+        pares+= parseInt (num1);
+    }
+    if (num2 % 2 != 0){
+        impares*=parseInt (num2);
+    } else {
+        pares+= parseInt (num2);
+    }
+    if (num3 % 2 != 0){
+        impares*=parseInt (num3);   
+    } else {
+        pares+= parseInt (num3);
+    }
+    if (num4 % 2 != 0){
+        impares*=parseInt (num4);
+    } else {
+        pares+= parseInt (num4);
     }
 }
