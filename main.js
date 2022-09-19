52c9d1181c9ccedb4d3a3f31733caa1d5145812f
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-18 21:53:13 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 addEventListener("DOMContentLoaded", (e) => {
-    let boton = document.querySelector("#form");
-    boton.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let pago = document.querySelector("#pago").value;
-        if (pago>130000){
-            let total = pago - (pago*0.15);
-            document.querySelector("#total").innerHTML = `El total a pagar es de ${total} y se le aplico un descuento del 15%`;
-        } else{
-            document.querySelector("#total").innerHTML = `El total a pagar es de ${pago}`;
-        }
+    let dato = document.querySelector("#dato");
+    dato.addEventListener("change", (e) => {
+        let seleccionado = dato.value;
+        let boton = document.querySelector("#form");
+        boton.addEventListener("submit", (e) =>{
+            e.preventDefault();
+            let sueldoEmp = pagar(seleccionado);
+            document.querySelector("#total").innerHTML = sueldoEmp;
+        })
     })
 })
+
+function pagar (seleccionado){
+    let horas = document.querySelector("#horas").value;
+    if  (seleccionado == "Planta"){
+        const sueldo = 20000;
+        let pagoTotal = sueldo*horas;
+        return pagoTotal;
+    } else if (seleccionado == "Administrativo"){
+        const sueldo = 10000;
+        let pagoTotal = sueldo*horas;
+        return pagoTotal;
+    } else{
+        alert("Debes seleccionar un cargo")
+    }
+}
