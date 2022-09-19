9620da97d975c2ad3e7611f0468ba639437800f2
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-18 22:51:15 -0500

    Ejercicio 12

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ addEventListener("DOMContentLoaded", (e) => {
     let boton = document.querySelector("#form");
     boton.addEventListener("submit", (e) => {
         e.preventDefault();
-        let nota1 = document.querySelector("#nota1").value;
-        let nota2 = document.querySelector("#nota2").value;
-        let nota3 = document.querySelector("#nota3").value;
-        let nota4 = document.querySelector("#nota4").value;
-        let nota5 = document.querySelector("#nota5").value;
-        let promedio = (parseInt(nota1)+parseInt(nota2)+parseInt(nota3)+parseInt(nota4)+parseInt(nota5))/5;
-        document.querySelector("#promedio").innerHTML = `La nota promedio de los estudiantes es de: ${promedio}`;
+        let num1 = document.querySelector("#num1").value;
+        let num2 = document.querySelector("#num2").value;
+        if (num1 > num2){
+            let res = num1-num2;
+            document.querySelector("#res").innerHTML = `El resultado de restar el primer numero y el segundo es ${res}`;
+        } else{
+            document.querySelector("#res").innerHTML = "La operacion no se puede hacer :("
+        }
+
     })
 })
