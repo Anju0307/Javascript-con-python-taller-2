085c0d37b6cf3dfbde6a96625cfaaffc4eeb8715
Anju0307 <jpmantilla01@misena.edu.co>
2022-09-18 22:45:57 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -2,16 +2,12 @@ addEventListener("DOMContentLoaded", (e) => {
     let boton = document.querySelector("#form");
     boton.addEventListener("submit", (e) => {
         e.preventDefault();
-        let pro1 = document.querySelector("#pro1").value;
-        let pro2 = document.querySelector("#pro2").value;
-        let pro3 = document.querySelector("#pro3").value;
-        let pro4 = document.querySelector("#pro4").value;
-        let pro5 = document.querySelector("#pro5").value;
-        let subtotal = parseInt(pro1)+parseInt(pro2)+parseInt(pro3)+parseInt(pro4)+parseInt(pro5);
-        let iva = subtotal*0.10;
-        let total = subtotal + iva;
-        document.querySelector("#subtotal").innerHTML=`El subtotal es: ${subtotal}`;
-        document.querySelector("#iva").innerHTML=`El iva es: ${iva}`;
-        document.querySelector("#total").innerHTML=`El total es: ${total}`;
+        let nota1 = document.querySelector("#nota1").value;
+        let nota2 = document.querySelector("#nota2").value;
+        let nota3 = document.querySelector("#nota3").value;
+        let nota4 = document.querySelector("#nota4").value;
+        let nota5 = document.querySelector("#nota5").value;
+        let promedio = (parseInt(nota1)+parseInt(nota2)+parseInt(nota3)+parseInt(nota4)+parseInt(nota5))/5;
+        document.querySelector("#promedio").innerHTML = `La nota promedio de los estudiantes es de: ${promedio}`;
     })
 })
